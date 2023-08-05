59cf22c7775998688bbb82ed892dccd4d0f75f02
JavierMedarde99 <javiermedmata@gmail.com>
2023-08-05 12:29:36 +0200

    Dia1

---
+++ Dia1/Primer_Proyecto.py
@@ -0,0 +1,17 @@
+print("Hola Mundo")
+
+print(100 + 50)
+
+print('Hola' + ' ' + 'Fede')
+
+print("Me llamo \"Federico\"")
+
+print("Esta es una linea\nY esta es otra linea")
+print("\tEsta es la tercera linea")
+
+print('This isn\'t a number')
+
+print("Estes signo \\ es una barra invertida")
+
+print("Tu nombre es : " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
+
---
+++ Dia1/Proyecto_Dia1.py
@@ -0,0 +1,1 @@
+print("La cerveza se llama \n\"" + input("¿Nombre de tu mascota?") + " " + input("¿Nombre de tu prima?") + "\"")
